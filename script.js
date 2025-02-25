5be80e1d345c06d677ecebe3a849f53e5ddb6546
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-02-25 14:09:00 +0800

    changed somethings

--- script.js
+++ script.js
@@ -1,65 +1,17 @@
-const spotlight = document.getElementById("spotlight");
-const projects = {
-  proj1: `<img src="Dependencies/CateringManagement.png" alt="">
-  <p>
-    3M’s Catering Management System is designed to lessen planning time
-    and scheduling for appointments, this system is programmed using HTML,
-    CSS, JavaScript, PHP and MySQL for data handling
-  </p>`,
-  proj2: `<img src="Dependencies/AirplaneTicketingCSharp.png" alt="" />
-  <p>
-    Airplane Ticketing System that my team created during our Object
-    Oriented Programming 2 programmed using C#, and MySQL for data
-    handling
-  </p>`,
-  proj3: `<img src="Dependencies/CashDenomination.png" alt="" />
-  <p>
-    This system is designed to determine the exact denominations of change
-    based on the available cash in the register. It is programmed in
-    Python.
-  </p>`,
-  proj4: `<img src="Dependencies/AirplaneTicketingReact.png" alt="" />
-  <p>
-    Migrated the Airplane Ticketing System to a different tech stack,
-    ReactJS, MySQL, ExpressJS, and NodeJS was used. This is still a work
-    in progress
-  </p>`,
-  proj5: `<img src="Dependencies/PitogoAppointment.png" alt="" />
-  <p>
-    Belonged to the team that created the Pitogo Virtual Appointment System in which users will be able to get paper works done online. This system is programmed using HTML, CSS, JavaScript, and Google API for Google Sheets 
-  </p>`,
-};
 
 let currentSpotlight = 3;
 const eventListeners = document.querySelectorAll(".img-eventlistener");
-const changeSpotlight = (e, source) => {
-  const parentDiv = e.target.closest(".img-eventlistener");
+const toRight = document.querySelector("div.to-right")
+const toLeft = document.querySelector("div.to-left")
 
-  const eventlistenerClass = Array.from(parentDiv.classList).find((className) =>
-    className.includes("content")
-  );
 
-  const contentNumber = eventlistenerClass.substring(7);
-  const toAdd = currentSpotlight - parseInt(contentNumber);
-  console.log(contentNumber);
+const changeSpotlight = (spotlight) => {
+  currentSpotlight += spotlight;
+  console.log(currentSpotlight)  
+}
 
-  eventListeners.forEach((eventListener) => {
-    const currentEventlistener = parseInt(
-      eventListener.classList[2].substring(7)
-    );
-    eventListener.classList.remove("content" + currentEventlistener);
-
-    let finalContentNumber = ((currentEventlistener + toAdd - 1 + 5) % 5) + 1;
-
-    eventListener.classList.add("content" + finalContentNumber);
-  });
-
-  spotlight.innerHTML = projects[source];
-  spotlight.classList.forEach((className) => {
-    spotlight.classList.remove(className);
-  });
-  spotlight.classList.add(source);
-};
+toRight.addEventListener("click", changeSpotlight(1))
+toLeft.addEventListener("click", changeSpotlight(-1))
 
 const navigation = document.querySelector("aside");
 const ShowNavigation = () => {
